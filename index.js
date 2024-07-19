7f1e0d7bf54f1f6171268558964785aa2e17faa2
joshua <joshuakruger66@live.com>
2024-07-19 22:09:16 +0200

    Debugged the DOM to not show duplicate values.

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ const addNewGoal = () => {
     
     // ⚠️ Hint 1: Check for duplicates
     // Use 'goalList' to get all existing goals and check if 'goalInput' matches any of them.
-    
+    if () {
+        result = 'Valid Syntax'
+    } else {
+        result = 'Invalid Syntax'
+    }
+
     // ⚠️ Hint 2: Prevent duplicates
     // If a duplicate is found, display an alert to the user and don't add the goal to the list.
     // If it's not a duplicate, proceed with adding it as a new goal.
